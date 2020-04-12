9a800082d9e98f4fbe26eeb184917d7d168d6e7c
fushuai-fs <fushuai_f@126.com>
2020-04-12 11:28:02 +0800

    test

---
+++ web/src/main/java/hotel/manage/web/controller/IndexController.java
@@ -0,0 +1,13 @@
+/*
+    CreateUser:fushuai
+    CreateDate:
+*/
+package hotel.manage.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+public class IndexController {
+}
