4472f0338fd97563ed902f33c63cb03104c33691
fushuai-fs <fushuai_f@126.com>
2020-04-12 10:58:06 +0800

    初始化

---
+++ web/src/test/java/hotel/manage/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package hotel.manage.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
