d390c81e7ff822d2923c45b93dd08043fb2e9a9e
fushuai-fs <fushuai_f@126.com>
2020-04-19 18:41:40 +0800

    test++

--- web/src/test/java/hotel/manage/web/WebApplicationTests.java
+++ web/src/test/java/hotel/manage/web/WebApplicationTests.java
@@ -6,8 +6,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+
+        int a=2;
+        int b=a++ << ++a + ++a;
+        System.out.println(b);
+    }
+
+    @Test
+    void contextLoads2() {
+
+        int a=2;
+        ++a;
+        System.out.println(a);
+        ++a;
+        System.out.println(a);
+        int c=a+a;
+        int b=a++<<c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
 
 }
